7e870136e6c37153a0732585b5c2a495f79d296d
sfershmiss11 <sfershmiss11@163.com>
2020-03-02 16:19:50 +0800

    ...

--- source/gallery/index.md
+++ source/gallery/index.md
@@ -3,27 +3,34 @@ title: 画廊
 date: 2020-03-01 19:20:54
 comments: false
 ---
+<center>爱一个人愿意把最珍贵的东西给她</center>
+<center>我穷但我会给钱你花</center>
+<center>我忙但我会陪着你。</center>
+<center style="margin: 100px;">.+†+.</center>
+</br>
 <div class="gallery-page">
 	<div class="gallery-list">
 		<div class="gallery-column">
 			<div class="gallery-item">
-				<a href="img/wife"><img src="https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife.png">
+				<a href="img/wife"><img src="https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife1.png">
 				</a>
-				<p>- ！相册1 -</p>
-			</div>
-			<div class="gallery-item">
-				<a href="img/wife2"><img src="https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife2.jpg">
-				</a>
-				<p>- ！相册2 -</p>
+				<p >-不畏人海拥挤-</p>
 			</div>
 		</div>
 		<div class="gallery-column">
 			<div class="gallery-item">
-				<a href="sample"><img src="https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife3.jpeg">
+				<a href="img/wife2"><img src="https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife3.jpg">
 				</a>
-				<p>- 相册名3 -</p>
+				<p >-你若安好-</p>
 			</div>
 		</div>
+		<div class="gallery-column">
+			<div class="gallery-item">
+				<a href="sample"><img src="https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife2.png">
+				</a>
+				<p >-愿得一人心-</p>
+			</div>
 		</div>
 	</div>
 </div>
+<center style="margin: 200px;width: 500px">.+ﾟ♪ﾟ+.ｏ.+ﾟ♪ﾟ+.ｏ.+ﾟ♪ﾟ+.ｏ.+ﾟ♪ﾟ+.ｏ.+ﾟ♪ﾟ+.ｏ.+ﾟ♪ﾟ+.</center>
--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -7,49 +7,32 @@ const ap = new APlayer({
         name: '勇气',
         artist: '棉子',
         url: 'http://music.163.com/song/media/outer/url?id=1411358329.mp3',
-        cover: '/images/5.jpg',
-      },
-      {
-        name: "星星",
-        artist: '张杰',
-        url: 'http://music.163.com/song/media/outer/url?id=1399004693.mp3',
-        cover: '/images/xingxing.jpg',
+        cover: 'https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife.png',
       },
       {
         name: '这就是爱吗',
         artist: '十豆',
         url: 'http://music.163.com/song/media/outer/url?id=1412242872.mp3',
-        cover: '/images/zjsam.jpg',
+        cover: 'https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife.png',
       },
-      {
-        name: 'Alone',
-        artist: 'Alan Walker / Noonie Bao',
-        url: 'http://music.163.com/song/media/outer/url?id=444269135.mp3',
-        cover: '/images/alone.jpg',
+
+	  {
+        name: '彼女は旅に出る',
+        artist: '鎖那',
+        url: 'http://music.163.com/song/media/outer/url?id=509106775.mp3',
+        cover: 'https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife.png',
       },
 	  {
-        name: 'Umbrella (Matte Remix)',
-        artist: 'Matte / Ember Island',
-        url: 'http://music.163.com/song/media/outer/url?id=164209623.mp3',
-        cover: '/images/1.jpg',
+        name: '失眠飞行',
+        artist: '接个吻，开一枪 / 沈以诚 / 薛明媛',
+        url: 'http://music.163.com/song/media/outer/url?id=1365898499.mp3',
+        cover: 'https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife.png',
       },
 	  {
-        name: '大眠 (完整版)原唱：王心凌',
-        artist: '小乐哥',
-        url: 'http://music.163.com/song/media/outer/url?id=3778678.mp3',
-        cover: '/images/2.jpg',
-      },
-	  {
-        name: '世间美好与你环环相扣',
-        artist: '柏松',
-        url: 'http://music.163.com/song/media/outer/url?id=1363948882.mp3',
-        cover: '/images/3.jpg',
-      },
-	  {
-        name: '飞',
-        artist: '王恩信Est / 二胖u',
-        url: 'http://music.163.com/song/media/outer/url?id=1386259535.mp3',
-        cover: '/images/4.jpg',
+        name: '冬眠',
+        artist: '司南',
+        url: 'http://music.163.com/song/media/outer/url?id=1398663411.mp3',
+        cover: 'https://raw.githubusercontent.com/sfershmiss11/Photo/master/wife.png',
       }
     ]
 });
